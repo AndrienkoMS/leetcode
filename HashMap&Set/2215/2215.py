60424ae25ac7de36dffa1cca6909f1a72d5e0fe9
andrienkoms <andrienkoms@gmail.com>
2025-04-09 23:29:45 +0300

    2215

--- HashMap&Set/2215/2215.py
+++ HashMap&Set/2215/2215.py
@@ -8,15 +8,16 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         exclusive1, exclusive2 = set(), set()  # only present in nums1 & only present in nums2
-        nums1 = sorted(nums1)
-        nums2 = sorted(nums2)
+        nums1 = sorted(set(nums1))
+        nums2 = sorted(set(nums2))
+        i = 0
         j = 0
 
-        for i in range(len(nums1)):
+        while i < len(nums1) and j < len(nums2):
             if nums1[i] == nums2[j]:
                 i += 1
                 j += 1
-            if nums1[i] > nums2[j]:
+            elif nums1[i] > nums2[j]:
                 exclusive2.add(nums2[j])
                 j += 1
             else:
@@ -27,6 +28,10 @@ class Solution(object):
             exclusive2.add(nums2[j])
             j += 1
 
+        while i < len(nums1):
+            exclusive1.add(nums1[i])
+            i += 1
+
         return [list(exclusive1), list(exclusive2)]
 
 
@@ -34,7 +39,7 @@ class test_solution(unittest.TestCase):
     def test_findDifference(self, ):
         solution = Solution()
         test_cases = [
-            ([1, 2, 3], [2, 4, 6], [[1, 3], [4, 6]]),
+            # ([1, 2, 3], [2, 4, 6], [[1, 3], [4, 6]]),
             ([1, 2, 3, 3], [1, 1, 2, 2], [[3], []])
         ]
         for nums1, nums2, expected in test_cases:
